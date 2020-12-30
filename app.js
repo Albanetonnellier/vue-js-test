ccb9a33ae9daa0b4539065f85d6d649012d53c77
albane tonnellier <albanetonnellier@gmail.com>
2020-12-30 16:52:25 +0000

    data biding link added

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ new Vue({
 	data: {
 		name: 'Albane',
 		job: 'Dev',
+		website: "https://www.youtube.com/watch?v=xIOwFTCBBDg&list=PL4cUxeGkcC9gQcYgjhBoeQH7wiAyZNrYa&index=5"
 	}
 	methods: {
 		greet: function(time){
