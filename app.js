e483cda4f85425d39ee109cf4b3a1fe2dc86f44d
albane tonnellier <albanetonnellier@gmail.com>
2020-12-31 16:09:25 +0000

    method added for cp

--- app.js
+++ app.js
@@ -5,16 +5,26 @@ new Vue({
 		job: 'Dev',
 		website: "https://www.youtube.com/watch?v=xIOwFTCBBDg&list=PL4cUxeGkcC9gQcYgjhBoeQH7wiAyZNrYa&index=5",
 		age: '24'
+		a: 0,
+		b: 0
 	}
 	methods: {
 		greet: function(time){
 			return 'Good' + time; + '' + this.name;
-		}
+		},
 		add: function(){
 			this.age++;
-		}
+		},
 		substract: function(){
 			this.age--;
+		},
+		addToA: function(){
+			console.log('addToA');
+			return this.a + this.age;
+		},
+		addToB: function(){
+			console.log('addToB');
+			return.this.b + this.age;
 		}
 	}
 	});
